70b0444f0c0f5085878022a97c7f564fd59f411f
Luqun Lou <luqunl@microsoft.com>
2018-10-09 18:14:18 +0000

    simple repro

--- tests/src/Interop/StringMarshalling/BSTR/PinvokeDef.cs
+++ tests/src/Interop/StringMarshalling/BSTR/PinvokeDef.cs
@@ -15,75 +15,14 @@ namespace NativeDefs
 	public int _pedding;
         [MarshalAs(UnmanagedType.BStr)]
         public string name;
-/*
-        public override bool Equals(object obj)
-        {
-            if(obj == null)
-                return false;
-            if(this.GetType() != obj.GetType())
-                return false;
-            Person anotherPerson = (Person)obj;
-            return this.age == anotherPerson.age && this.name == anotherPerson.name;
-        }
-
-        public override int GetHashCode() => (age, name).GetHashCode();
-
-        public override string ToString()
-        {
-            return name + ":" + age.ToString();
-        }i
-*/
     }
-
-    [return: MarshalAs(UnmanagedType.BStr)]
-    public delegate string Del_MarshalPointer_Out([MarshalAs(UnmanagedType.BStr)] out string s);
-    
-    [return: MarshalAs(UnmanagedType.BStr)]
-    public delegate string Del_Marshal_InOut([MarshalAs(UnmanagedType.BStr)]string s);
-    
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-    [return: MarshalAs(UnmanagedType.BStr)]
-    public delegate string DelMarshalPointer_Out([MarshalAs(UnmanagedType.BStr)][Out] out string s);
-
-    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
-    [return: MarshalAs(UnmanagedType.BStr)]
-    public delegate string DelMarshal_InOut([MarshalAs(UnmanagedType.BStr)][In, Out]string s);
-
-    public delegate bool DelMarshal_Struct_In(Person person);
 
     public static class PInvokeDef
     {
         public const string NativeBinaryName = "BSTRTestNative";
 
         [DllImport(NativeBinaryName)]
-        [return: MarshalAs(UnmanagedType.BStr)]
-        public static extern string Marshal_InOut([In, Out][MarshalAs(UnmanagedType.BStr)]string s);
-
-        [DllImport(NativeBinaryName)]
-        [return: MarshalAs(UnmanagedType.BStr)]
-        public static extern string Marshal_Out([Out][MarshalAs(UnmanagedType.BStr)]string s);
-
-        [DllImport(NativeBinaryName)]
-        [return: MarshalAs(UnmanagedType.BStr)]
-        public static extern string MarshalPointer_InOut([MarshalAs(UnmanagedType.BStr)]ref string s);
-
-        [DllImport(NativeBinaryName)]
-        [return: MarshalAs(UnmanagedType.BStr)]
-        public static extern string MarshalPointer_Out([MarshalAs(UnmanagedType.BStr)]out string s);
-
-        [DllImport(NativeBinaryName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool RPinvoke_DelMarshal_InOut(DelMarshal_InOut d, [MarshalAs(UnmanagedType.BStr)]string s);
-
-        [DllImport(NativeBinaryName, CallingConvention = CallingConvention.StdCall)]
-        public static extern bool RPinvoke_DelMarshalPointer_Out(DelMarshalPointer_Out d);
-        
-        [DllImport(NativeBinaryName)]
         public static extern bool Marshal_Struct_In(Person person);
 
-        [DllImport(NativeBinaryName)]
-        public static extern bool MarshalPointer_Struct_InOut(ref Person person);
-
-        [DllImport(NativeBinaryName)]
-        public static extern bool RPInvoke_DelMarshal_Struct_In(DelMarshal_Struct_In person);
     }
 }
